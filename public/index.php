78a2b92bf2d3c8768b9632bc0a1fe5fa9aac855f
Jonathan <jonathan.rbm01@gmail.com>
2024-10-01 13:21:10 +0200

    Prueba carrito

--- public/index.php
+++ public/index.php
@@ -1,11 +1,26 @@
-<!doctype html>
+<?php
+include "Carrito/carrito.php";
+include "Carrito/Producto.php";
+ini_set('display_errors', 1);
+?>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Document</title>
+ <meta charset="UTF-8">
+ <title>Carrito de la compra</title>
+ <link rel="stylesheet" href="estilo.css">
 </head>
 <body>
-<h1>Hola</h1>
+ <?php
+ $p1 = new Producto("Espuma de afeitar", 3.5);
+ $p2 = new Producto("Cereales bolas de chocolate", 5.99);
+ $p3 = new Producto("Servilletas 20x20", 1.2);
+ $carrito = new Carrito();
+ $carrito->meter($p1);
+ $carrito->meter($p2);
+ $carrito->meter($p3);
+
+ $carrito->mostrar();
+ ?>
 </body>
 </html>
